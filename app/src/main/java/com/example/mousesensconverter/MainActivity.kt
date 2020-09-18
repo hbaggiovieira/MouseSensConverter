7427d627575c4da7461da0bd68a9467b6ede9927
HenriqueVieira <hbaggiovieira@gmail.com>
2020-09-17 22:29:56 -0300

    Hotfixed formula

--- app/src/main/java/com/example/mousesensconverter/MainActivity.kt
+++ app/src/main/java/com/example/mousesensconverter/MainActivity.kt
@@ -30,38 +30,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     //Função calcular
     private fun calculate() {
-        val dpiConstant = editDpi1.text.toString().toFloat() / editDpi2.text.toString().toFloat()
+
         val dpi1 = editDpi1.text.toString()
         val dpi2 = editDpi2.text.toString()
         val sensCs = editSensCS.text.toString()
         val sensVal = editSensValorant.text.toString()
         val SENS_CONSTANT = 3.18f
 
-        if (dpi2.isEmpty()) {
+        if (dpi2 == "0") {
             Toast.makeText(this, "Não é possível inserir valor 0!", Toast.LENGTH_LONG)
                 .show()
         } else {
 
             try {
-                when {
-                    //Cálculo sem dpi
-                    sensVal.isEmpty() && (dpi1.isEmpty() || dpi2.isEmpty()) -> textResult.text =
-                        "Sens: ${"%.4f".format(sensCs.toFloat() / SENS_CONSTANT)}"
-                    sensCs.isEmpty() && (dpi1.isEmpty()|| dpi2.isEmpty()) -> textResult.text =
-                        "Sens:${"%.4f".format(sensVal.toFloat() * SENS_CONSTANT)}"
+                //CS -> Valorant sem dpi
+                if (dpi1.isEmpty() || dpi2.isEmpty() && sensCs.isNotEmpty()) {
+                    val result1 = (sensCs.toFloat() / SENS_CONSTANT)
+                    textResult.text = "Sens: ${"%.4f".format(result1)}"
+                }
+                //Valorant -> CS sem dpi
+                else if (dpi1.isEmpty()|| dpi2.isEmpty() && sensVal.isNotEmpty()) {
+                    val result2 = (sensVal.toFloat() * SENS_CONSTANT)
+                    textResult.text = "Sens: ${"%.4f".format(result2)}"
 
-                    //Cálculo com dpi
-                    (dpi1.isNotEmpty() && dpi2.isNotEmpty()) && sensVal.isEmpty() -> textResult.text =
-                        "Sens: ${"%.4f".format(dpiConstant * (sensCs.toFloat() / SENS_CONSTANT))}"
-                    (dpi1.isNotEmpty() && dpi2.isNotEmpty()) && sensCs.isEmpty()-> textResult.text =
-                        "Sens: ${"%.4f".format(dpiConstant * (sensVal.toFloat() * SENS_CONSTANT))}"
+                    //CS -> Valorant com dpi
+                } else if (dpi1.isNotEmpty() && dpi2.isNotEmpty() && sensCs.isNotEmpty()) {
+                    val result3 = ((dpi1.toFloat() / dpi2.toFloat()) * (sensCs.toFloat() / SENS_CONSTANT))
+                    textResult.text = "Sens: ${"%.4f".format(result3)}"
 
-                    else -> Toast.makeText(
+                    //Valorant -> CS com dpi
+                } else if (dpi1.isNotEmpty() && dpi2.isNotBlank() && sensVal.isNotEmpty()) {
+                    val result4 = ((dpi1.toFloat() / dpi2.toFloat()) * (sensVal.toFloat() * SENS_CONSTANT))
+                    textResult.text = "Sens: ${"%.4f".format(result4)}"
+                } else {
+                    Toast.makeText(
                         this,
                         "Verifique os valores inseridos e tente novamente!",
                         Toast.LENGTH_LONG
-                    ).show()
+                    )
+                        .show()
                 }
+
             } catch (nfe: NumberFormatException) {
                 Toast.makeText(
                     this,
@@ -70,42 +79,5 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 ).show()
             }
         }
-
     }
 }
-
-
-//Another way of doing it
-/*try {
-            //CS -> Valorant sem dpi
-            if (dpi1 == "" || dpi2 == "" && sensCs != "") {
-                val result1 = (sensCs.toFloat() / 3.18f)
-                textResult.text = "Sens: ${"%.4f".format(result1)}"
-            }
-            //Valorant -> CS sem dpi
-            else if (dpi1 == "" || dpi2 == "" && sensVal != "") {
-                val result2 = (sensVal.toFloat() * 3.18f)
-                textResult.text = "Sens: ${"%.4f".format(result2)}"
-
-                //CS -> Valorant com dpi
-            } else if (dpi1 != "" && dpi2 != "" && sensCs != "") {
-                val result3 = ((dpi1.toFloat() / dpi2.toFloat()) * (sensCs.toFloat() / 3.18f))
-                textResult.text = "Sens: ${"%.4f".format(result3)}"
-
-                //Valorant -> CS com dpi
-            } else if (dpi1 != "" && dpi2 != "" && sensVal != "") {
-                val result4 = ((dpi1.toFloat() / dpi2.toFloat()) * (sensVal.toFloat() * 3.18f))
-                textResult.text = "Sens: ${"%.4f".format(result4)}"
-            } else {
-                Toast.makeText(
-                    this,
-                    "Verifique os valores inseridos e tente novamente!",
-                    Toast.LENGTH_LONG
-                )
-                    .show()
-            }
-
-        } catch (nfe: NumberFormatException) {
-            Toast.makeText(this, "Informar valores válidos!", Toast.LENGTH_LONG)
-                .show()
-        }*/
